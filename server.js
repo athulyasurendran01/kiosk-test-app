ab7015b73cad0514adda212a26fb8dd370254edf
Athulya P <Athulya@Bridge.local>
2017-10-27 10:41:18 +0530

    2

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ var https = require('https');
 
 var argv = minimist(process.argv.slice(2), {
   default: {
-      as_uri: "https://localhost:5656/",
+      as_uri: "https://localhost:6656/",
       ws_uri: "ws://localhost:8888/kurento"
   }
 });
